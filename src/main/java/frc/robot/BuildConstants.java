103483e2dd80f29f66a57fe0f396b6ea50f6f800
bmxcmx <74631712+bmxcmx@users.noreply.github.com>
2024-04-09 13:29:46 -0400

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRCTeam343AdvKitTest";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "cc411a06f11d435f2e6acf3faafda0870b4c06e6";
+  public static final String GIT_DATE = "2024-04-08 02:14:30 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-04-08 11:43:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1712590991779L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
